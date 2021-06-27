fd82c464a2f75b511cca222fdeadfb184d830765
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-06-27 12:28:27 +0200

    add unit test for case with specific derived data path

--- spec/docc_action_spec.rb
+++ spec/docc_action_spec.rb
@@ -1,9 +1,15 @@
 describe Fastlane::Actions::DoccAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("xcodebuild docbuild -scheme test")
+    it 'generate docc without optinal arguments' do
+      expect(Fastlane::UI).to receive(:message).with("xcodebuild docbuild -scheme testScheme")
 
-      Fastlane::Actions::DoccAction.run(scheme: "test")
+      Fastlane::Actions::DoccAction.run(scheme: "testScheme")
+    end
+
+    it 'generate docc with specific derived data path' do
+      expect(Fastlane::UI).to receive(:message).with("xcodebuild docbuild -scheme test -derivedDataPath testPath")
+
+      Fastlane::Actions::DoccAction.run(scheme: "test", derived_data_path: "testPath")
     end
   end
 end

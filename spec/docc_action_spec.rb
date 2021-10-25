876acb013e888c524c2055845a0b3cef9ed38c09
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-10-25 19:08:10 +0200

    add unit tests for the new paramater

--- spec/docc_action_spec.rb
+++ spec/docc_action_spec.rb
@@ -11,5 +11,17 @@ describe Fastlane::Actions::DoccAction do
 
       Fastlane::Actions::DoccAction.run(scheme: "test", derived_data_path: "testPath")
     end
+
+    it 'generate docc with specific destination' do
+      expect(Fastlane::UI).to receive(:message).with("xcodebuild docbuild -scheme test -destination generic/platform=macOS")
+
+      Fastlane::Actions::DoccAction.run(scheme: "test", destination: "generic/platform=macOS")
+    end
+
+    it 'generate docc with specific derived data path and destination' do
+      expect(Fastlane::UI).to receive(:message).with("xcodebuild docbuild -scheme test -derivedDataPath testPath -destination generic/platform=macOS")
+
+      Fastlane::Actions::DoccAction.run(scheme: "test", derived_data_path: "testPath", destination: "generic/platform=macOS")
+    end
   end
 end
